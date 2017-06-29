e9616dee312a36a0857768fee5aa2b27ce48e24c
jr09 <jiveshrajpal09@gmail.com>
2017-06-29 00:24:11 -0700

    Fixed duplicate user issue, added chat panel feature

--- MEAN/ChatRoom/server.js
+++ MEAN/ChatRoom/server.js
@@ -1,3 +1,5 @@
+// add all required packages
+
 var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
@@ -10,38 +12,57 @@ app.get('/', function(request, response){
     response.render('users');
 });
 
+// start listening on local host
+
 var server = app.listen(8000, function(){
     console.log('Listening on port 8000 of local host!:)');
 });
 
 var io = require('socket.io').listen(server);
 
+// create array of users
+
 var users = [];
+
+// create dictionary of users for checking duplicate user names
+
 var users_dictionary = {};
+
+// create array of chats
+
+var chats = [];
 
 io.sockets.on('connection', function(socket){
     console.log('We are using sockets!');
     console.log(socket.id);
 
+// add new user to array and dictionary, emit to all active users the new user
+
     socket.on('users_new_user', function(data){
         users.push(data.name);
         users_dictionary[data.name] = data.name;
-        console.log(users);
-        console.log(users_dictionary);
+        print_users();
         socket.emit('users_all_users', {users: users});
+        socket.emit('all_messages', {messages: chats});
         socket.broadcast.emit('users_add_user', {new_user: data.name});
     });
+
+// on disconnect, emit the user name to be removed to all active users
 
     socket.on('disconnect_user', function(data){
         console.log(`Request  to remove ${data.name}`);
         var name_index = users.indexOf(data.name);
         users.splice(name_index, 1);
+        delete users_dictionary[data.name];
+        print_users();
         socket.broadcast.emit('users_remove_user', {remove_user: data.name});
     });
 
+// handle user name checking, if user name already taken send false else true
+
     socket.on('check_user_name', function(data){
         console.log(`Request to check availability of ${data.name} received!!`);
-        console.log(users_dictionary);
+        print_users();
         if(users_dictionary[data.name]){
             socket.emit('user_name_validity', {result: false});
         }
@@ -49,4 +70,18 @@ io.sockets.on('connection', function(socket){
             socket.emit('user_name_validity', {result: true});
         }
     });
+
+// handle message received from active user, send to all active users
+
+    socket.on('message_from_user', function(data){
+        console.log(`Received ${data.message} from user ${data.name}`);
+        chats.push(data.message);
+        io.emit('new_message', {message: data.message});
+    });
 });
+
+
+function print_users(){
+    console.log(users);
+    console.log(users_dictionary);
+}
--- MEAN/ChatRoom/static/users_js.js
+++ MEAN/ChatRoom/static/users_js.js
@@ -1,55 +1,60 @@
+// create use rname and custom prompt message
+
 var user_name = '';
-var user_name_valid = false;
 var prompt_message = 'Please enter a user name to continue';
 
-function get_user_name(){
-    while(user_name.length == 0){
+// connect the socket
+
+var socket = io.connect('http://localhost:8000');
+
+// This function requests for a user name
+
+function get_user_name(callback){
+    do{
         user_name = prompt(prompt_message);
         prompt_message = 'User name cannot be blank';
     }
+    while(!user_name || user_name.trim().length === 0);
+    callback();
+}
+
+// This function emits to server to check if user name is already taken
+
+function validate_user_name(){
+    socket.emit('check_user_name', {name: user_name});
 }
 
 $(document).ready(function(){
 
-    var socket = io.connect();
+// For a vlid user name, inform server to add this user else re-prompt valid for user name
 
     socket.on('user_name_validity', function(data){
-        alert(`Received ${data.result} from server!`);
-        if(data.result == true){
-            user_name_valid = true;
+        console.log(`Received ${data.result} from server!`);
+        if(data.result){
+            socket.emit('users_new_user', {name: user_name});
         }
         else{
             prompt_message = 'User name already taken, please enter a different user name';
+            get_user_name(validate_user_name);
         }
     });
 
-    while(!user_name_valid){
-        get_user_name();
-        socket.emit('check_user_name', {name:user_name});
-    }
-
-    socket.emit('users_new_user', {name: user_name});
-
-    socket.on('user_name_validity', function(data){
-        alert(`Received ${data.result} from server!`);
-        if(data.result == true){
-            user_name_valid = true;
-        }
-        else{
-            prompt_message = 'User name already taken, please enter a different user name';
-        }
-    });
+// add all the active users received from server when page is loaded for the first time
 
     socket.on('users_all_users', function(data){
         console.log('im here');
         for(let idx=0; idx<data.users.length; idx++){
-            $('body').append('<div class="users" id='+data.users[idx]+'><h3>'+data.users[idx]+'</h3></div>');
+            $('#users_panel').append('<div class="users" id='+data.users[idx]+'><h3>'+data.users[idx]+'</h3></div>');
         }
     });
 
+// add a new user when nitified by server
+
     socket.on('users_add_user', function(data){
-        $('body').append('<div class="users" id='+data.new_user+'><h3>'+data.new_user+'</h3></div>');
+        $('#users_panel').append('<div class="users" id='+data.new_user+'><h3>'+data.new_user+'</h3></div>');
     });
+
+// remove user from list of active user html when they disconnect
 
     socket.on('users_remove_user', function(data){
         var remove_name = '#' + data.remove_user;
@@ -57,9 +62,40 @@ $(document).ready(function(){
         $(remove_name).remove();
     });
 
+// notify server the user name who is about to disconnect
+
     $(window).on('beforeunload', function(){
         if(user_name){
             socket.emit('disconnect_user', {name:user_name});
         }
     });
+
+// when user clicks 'Post' send message string to server
+
+    $('button').click(function(){
+        if($.trim($('textarea').val())){
+            var message_text = $('textarea').val();
+            var message_string = user_name + '  :' + message_text;
+            socket.emit('message_from_user', {name: user_name, message: message_string});
+            $('textarea').val("");
+        }
+    });
+
+// get new message from server and update chat
+
+    socket.on('new_message', function(data){
+        console.log(`Recieved ${data.message} from server!`);
+        $('#chat_panel').append('<p>'+data.message+'</p>');
+    });
+
+// when user loads page first time, load all previous chats
+
+    socket.on('all_messages', function(data){
+        console.log(`Received all of the old chat from server!`);
+        for(let idx = 0; idx < data.messages.length; idx++){
+            $('#chat_panel').append('<p>'+data.messages[idx]+'</p>');
+        }
+    });
+
+    get_user_name(validate_user_name);
 });
